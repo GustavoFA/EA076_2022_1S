b32c36fee7830a47a54abf95fa293f37418f21d5
Jitesh Ashok <76069407+Jitesh193@users.noreply.github.com>
2022-04-15 12:31:43 -0300

    Update projeto_2_cont_vent.ino

--- Projeto 2/projeto_2_cont_vent.ino
+++ Projeto 2/projeto_2_cont_vent.ino
@@ -25,16 +25,7 @@ int tam_msg = 0;
 // Variável para identificar que a mensagem foi devidamente recebida
 bool msg_recebida = 0;
 
-
-// Strings
-
-String codigo1 = "VENT";
-String codigo2 = "EXAUST";
-String codigo3 = "PARA";
-String codigo4 = "RETVEL";
-String codigo5 = "VEL ";
-
-String vel = "";
+String vel = ""; // String que armazena o valor da velocidade do comando "VEL"
 
 void setup() {
   Serial.begin(9600); 
@@ -42,17 +33,6 @@ void setup() {
 
 void loop() {
 
-    
-  // Função usada para checar se a fun_receber estava ok
-  /*
-  if(fun_receber()){
-    msg_recebida = 0;
-    for(int j=0; j<tam_msg;j++){
-      Serial.print(mensagem[j]);
-    }
-    Serial.println();
-  }
-  */
   fun_receber();
   fun_deco();
 
@@ -92,63 +72,75 @@ bool fun_receber(){
       // e como não há nenhum comando com mais de 8 bytes
       if(cont_vet > 7) cont_vet = 0;
     }
-    
-    fun_deco();
   }
 }
 
+// Função que decodifica a mensagem que foi enviada ao monitor, e para o caso de setar a velocidade, retorna o valor da velocidade
 void fun_deco() {
-    
+    // Verifica se a variavel de sinalizacao de mensagem foi setada
     if(msg_recebida) {
         
-        msg_recebida = 0;
-      	String codigo = "";
-      	 
+        msg_recebida = 0; // Zera a variavel de sinalizacao antes de fazer a decodificacao
+      	String codigo = ""; 
+      	// Armazena os elementos do buffer dentro da variavel do tipo String para manipulacoes futuras
         for(int j=0; j<tam_msg;j++){
-          //Serial.print(mensagem[j]);
           codigo.concat(mensagem[j]);
         }
-      	//Serial.print(codigo);
-        //Serial.println();
         
-        if (codigo.equalsIgnoreCase(codigo1)) {
+        // Verifica qual comando foi escrito no monitor serial, para enviar a UART sua respectiva mensagem (de erro ou nao)
+        if (codigo.equalsIgnoreCase("VENT")) {
             Serial.print("OK VENT");
           	Serial.println();
         }
-        else if (codigo.equalsIgnoreCase(codigo2)) {
+        else if (codigo.equalsIgnoreCase("EXAUST")) {
             Serial.print("OK EXAUST");
           	Serial.println();
         }
-        else if (codigo.equalsIgnoreCase(codigo3)) {
+        else if (codigo.equalsIgnoreCase("PARA")) {
             Serial.print("OK PARA");
           	Serial.println();
         }
-        else if (codigo.equalsIgnoreCase(codigo4)) {
+        else if (codigo.equalsIgnoreCase("RETVEL")) {
             Serial.print("VEL: X RPM");
           	Serial.println();
         }
-        // Comando de velocidade - identifica os caracteres VEL 
-        else if (codigo.substring(0, 4).equalsIgnoreCase(codigo5)) {
-            /*Terminar as outras condicionais*/
-            
-            vel = codigo.substring(4, 7);
-            if (vel.toInt() == 100) {
-                Serial.print("DEU CERTO");
-          	    Serial.println();
-            }
-            if (vel = " ") {
+        
+        /* Comando de velocidade - para esse comando, é feito a identificacao do comando "VEL" atraves dos 3 primeiros elementos da variavel 'codigo'
+           e, após isso, é verificado se a o numero que seta a velocidade do motor esta no formato xxx (xxx entre 000 e 100) */
+        else if (codigo.substring(0, 3).equalsIgnoreCase("VEL")) {
 
-              Serial.print("Vazio");
-              Serial.println();
+            codigo.remove(0,4); // Remove os 4 primeiros elementos do comando ('VEL '), para que seja trabalhado somente com os numeros
+
+            /* Verifica se o comando enviado esta no formato correto ('xxx'), e para isso, utiliza-se o length() para ver se o tamanho do comando
+               enviado corresponde com o tamanho do formato desejado */
+            if (codigo.length() == 3) {
+                
+                vel = codigo;
+                if (vel.toInt() >= 0 && vel.toInt() <= 100) {
+                    Serial.print("OK VEL ");
+                    Serial.print(vel);
+                    Serial.print("%");
+                    Serial.println();
+                    
+                    return vel.toInt();
+                }
+                else {
+                    Serial.print("ERRO: PARÂMETRO INCORRETO");
+                    Serial.println();
+                }
             }
-            
-            //Serial.print(vel);
-          	//Serial.println();
+            /* Caso o tamanho do comando enviado não corresponda ao tamanho desejado, é enviado a mensagem de parametro ausente */
+            else {
+                Serial.print("ERRO: PARÂMETRO AUSENTE");
+                Serial.println();
+            }
         }
+
+        /* Caso a mensagem enviada ao monitor serial não corresponda com nenhum dos casos anteriores ou que a mensagem extrapolou o tamanho 
+           definido (tamanho maximo do buffer é de 8 - contando com o *) é enviado a mensagem de erro de comando inexistente */ 
         else {
             Serial.print("ERRO: COMANDO INEXISTENTE");
             Serial.println();
         }
-    }
-    
+    } 
 }
