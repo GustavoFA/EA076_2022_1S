ffb3d6819aef51cd88fc9ad47c8baa533d854a23
Gustavo Freitas Alves <58485863+GustavoFA@users.noreply.github.com>
2022-06-13 15:43:43 -0300

    Create teclado_matricial.c

---
+++ Projeto 3/teclado_matricial.c
@@ -0,0 +1,19 @@
+// Atividade Parcial 2 - Teclado Matricial
+// Gustavo Freitas Alves          236249
+// Jitesh Ashok Manilal Vassaram  175867
+
+// Link Tinkercad: https://www.tinkercad.com/things/cTmmGuVzZMp-terrific-trug/editel?sharecode=gE3_BYuuf9oVNRcQMq7H1X0me_c7ptIK3DblF9cNT2c
+
+void setup(){
+
+    
+
+
+}
+
+void loop(){
+
+
+
+
+}
